9daf5749923bf4c35bb4a38e654ac5c585f82936
apinter <akos.pinter@ashszoftverhaz.hu>
2021-09-20 17:37:13 +0200

    First project setup

---
+++ medior-test/src/main/java/hu/futureofmedia/mediortest/MediorTestApplication.java
@@ -0,0 +1,13 @@
+package hu.futureofmedia.mediortest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediorTestApplication {
+
+    public static void main( String[] args ) {
+        SpringApplication.run(MediorTestApplication.class, args);
+    }
+
+}
